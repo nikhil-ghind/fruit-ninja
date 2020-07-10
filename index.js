84e86d359c38852317f85c83abeb2b4eb4859cc0
thePmSquare <impmmmm@gmail.com>
2020-07-11 05:12:39 +0530

    set up random movement

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
 const startButton = document.querySelector("#startButton");
 const pauseButton = document.querySelector("#pauseButton");
-startButton.addEventListener("click", () => {
-  let intervalid = setInterval(() => {
-    let img = document.createElement("img");
-    img.setAttribute("src", selectRandomImage());
-    img.classList.add("fruit");
-
-    img.addEventListener("mouseleave", (e) => {
-      e.target.remove();
-    });
-    document.body.appendChild(img);
-  }, 1000);
-  pauseButton.addEventListener("click", () => {
-    clearTimeout(intervalid);
-  });
-});
+const score = document.querySelector("#score");
+const lives = document.querySelector("#lives");
+// Change this to increase difficulty
+const numberOfNewImagesPerSecond = 1;
 function selectRandomImage() {
   const choices = [
     "apple",
@@ -29,4 +18,56 @@ function selectRandomImage() {
   ];
   let selectedFruit = choices[Math.floor(Math.random() * choices.length)];
   return `./images/${selectedFruit}.png`;
+}
+
+startButton.addEventListener("click", () => {
+  // Create image after every 1000ms.
+  let intervalid = setInterval(() => {
+    let img = document.createElement("img");
+    img.setAttribute("src", selectRandomImage());
+    img.classList.add("fruit");
+    document.body.appendChild(img);
+    //To create falling animation for fruits.
+    startMoving(img);
+  }, numberOfNewImagesPerSecond * 1000);
+  // Event listener for pause in every created image.
+  pauseButton.addEventListener("click", () => {
+    clearTimeout(intervalid);
+  });
+});
+
+function startMoving(img) {
+  img.style.top = Math.floor(Math.random() * 51) + "vh";
+  let initialTop = img.style.top;
+  if (initialTop === "0vh") {
+    img.style.left = Math.floor(Math.random() * 101) + "vw";
+  } else {
+    img.style.left =
+      [
+        0,
+        parseInt(getComputedStyle(document.body).width) -
+          parseInt(getComputedStyle(img).width),
+      ][Math.floor(Math.random() * 2)] + "px";
+  }
+  let initialLeft = img.style.left;
+  img.addEventListener("mouseleave", (e) => {
+    score.innerHTML = parseInt(score.innerHTML) + 1;
+    e.target.remove();
+  });
+  let finalTop =
+    parseInt(getComputedStyle(document.body).height) -
+    parseInt(getComputedStyle(img).height) +
+    "px";
+  let finalLeft = Math.floor(Math.random() * 61) + 20 + "vw";
+  let rate = Math.floor(Math.random() * 7) + 9 + "s";
+  img.style.transition = "all " + rate + " linear";
+  img.style.top = finalTop;
+  img.style.left = finalLeft;
+  img.addEventListener("transitionend", () => {
+    if (lives.innerHTML === "0") {
+      console.log("game over");
+    } else {
+      lives.innerHTML = parseInt(lives.innerHTML) - 1;
+    }
+  });
 }
